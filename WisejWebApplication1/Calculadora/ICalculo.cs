0ba902c30163c5a28ad3c3b40e9c383fa8a3a49e
SCHAD\jrtaveras <jrtaveras@schad.do>
2023-02-28 20:31:20 -0400

    Se corrigio develop

---
+++ WisejWebApplication1/Calculadora/ICalculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisejWebApplication1
+{
+    public interface ICalculo
+    {
+        Calculos Calculos { get; set; }
+        decimal Numero1 { get; set; }
+        decimal Numero2 { get; set; }
+        decimal Calcular();
+    }
+}
---
+++ WisejWebApplication1/Calculadora/Modules/CalcularModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+
+namespace WisejWebApplication1.Modules
+{
+    public class CalcularModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICalculo>().To<Suma>().InSingletonScope();
+            Bind<ICalculo>().To<Resta>().InSingletonScope();
+            Bind<ICalculo>().To<Multiplicacion>().InSingletonScope();
+            Bind<ICalculo>().To<Division>().InSingletonScope();
+        }
+
+    }
+}
